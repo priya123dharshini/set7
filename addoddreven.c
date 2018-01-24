2c8ca6e3e1916b522741246fcfefbc7925efe851
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-01-24 16:58:23 +0530

    Create addoddreven.c

---
+++ addoddreven.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+ int n1,n2,add,res;
+ printf("enter number n");
+ scanf("%d",&n);
+ printf("enter number k");
+ scanf("%d",&k);
+ add=n+k;
+ res=add%2;
+ if(res==0)
+ {
+  printf("the result is even",res);
+  }
+ else
+ {
+   printf("the result is odd",res);
+   }
+   
