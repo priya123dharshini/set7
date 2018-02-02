94aae43ece2d19caa981e50232180368054f6405
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-02 14:58:26 +0530

    Create kchar.c

---
+++ kchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{ 
+ char s[20],i;
+ int k;
+ printf("enter a string");
+ scanf("%c",&s[i]);
+printf("enter k");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+    printf("%c",s[i]);
+    }
+    }
+ 
+ 
