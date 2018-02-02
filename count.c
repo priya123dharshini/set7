f6ca19a6955e5c9d204ff5ee671d6a3c562a5ce8
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-02 13:56:41 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{ 
+ int i,count=0;
+ char s;
+ printf("enter a sentence");
+ scanf("%s",&s);
+ for(i=0;i="/n";i++)
+ {
+  if(s=="/")
+   {
+    count++;
+    }
+    printf("%d",count);
+    }
+    
